3148719180d2412200e5a36a89dee421ba5f11cd
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-01-27 13:13:21 +0200

    Upload: BashSoft - First Part - 1. Creating the basic functionality.

---
+++ BashSoft - First Part/BashSoft/IOManager.cs
@@ -0,0 +1,28 @@
+﻿namespace BashSoft
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    public static class IOManager
+    {
+        public static void TraverseFolder(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            int initialIdentity = path.Split('\\').Length;
+            Queue<string> subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+
+            while (subFolders.Count > 0)
+            {
+                string currentPath = subFolders.Dequeue();
+                int identention = currentPath.Split('\\').Length - initialIdentity;
+                OutputWriter.WriteMessageOnNewLine($"{new string('-', identention)}{currentPath}");
+
+                foreach (string directoryPath in Directory.GetDirectories(currentPath))
+                {
+                    subFolders.Enqueue(directoryPath);
+                }
+            }
+        }
+    }
+}
