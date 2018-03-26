c5137c18fee5ee805b3a6c9b06644fc17b9aa2e4
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-03-26 16:39:53 +0300

    Updated interface - IRequester.

--- BashSoft-ThirdPart/BashSoft/Contracts/IRequester.cs
+++ BashSoft-ThirdPart/BashSoft/Contracts/IRequester.cs
@@ -7,7 +7,7 @@
         void GetStudentsScoresFromCourse(string courseName, string studentName);
 
         void GetAllStudentsFromCourse(string courseName);
-
+        
         ISimpleOrderedBag<ICourse> GetAllCoursesSorted(IComparer<ICourse> cmp);
 
         ISimpleOrderedBag<IStudent> GetAllStudentsSorted(IComparer<IStudent> cmp);
